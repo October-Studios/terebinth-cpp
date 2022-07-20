52815cd79f0f04514ce3ffc3cea819017af6e8c9
Cameron Howell <crhowell3@gmail.com>
2022-07-19 22:50:56 -0500

    Fixed pointer issue

--- src/terebinth/type.cpp
+++ src/terebinth/type.cpp
@@ -231,7 +231,7 @@ class TupleType : public TypeBase {
     return {0, nullptr};
   }
 
-  auto GetAllSubTypes() -> std::vector<NamedType> { return &(*sub_types_); }
+  auto GetAllSubTypes() -> std::vector<NamedType> * { return &(*sub_types_); }
 
   auto IsWhatev() -> bool { return hasWhatev; }
 
